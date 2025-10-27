441ea1c28a415d11e46f64bbbe7a368b803d28f9
Talha Mahamud <95077791+talhamahamud@users.noreply.github.com>
2025-10-27 13:18:42 +0600

    Update Majority Element.py

--- Easy/Majority Element.py
+++ Easy/Majority Element.py
@@ -9,3 +9,16 @@ class Solution:
 
         # i learn about the collections and Counter by solving the previous
         #problem, it return the total number of i, in a list repeated.
+
+
+# This the another solution.
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        max_count=0
+        main=0
+        for i in set(nums):
+            if nums.count(i)>max_count:
+                max_count=nums.count(i)
+                main=i
+                
+        return main
